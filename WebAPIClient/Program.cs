5ea5c51f0265d7389b6df87805dd5c18902fd6e9
Friyank Parikh <pppriyankparikh@gmail.com>
2017-07-06 20:25:21 +0530

    Adding new Class librry

--- WebAPIClient/Program.cs
+++ WebAPIClient/Program.cs
@@ -1,15 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using WebApiHelper;
 
 namespace WebAPIClient
 {
-    class Program
+    class Caller
     {
+        private static string serviceUrl = "https://fapple.crm.dynamics.com/";  //CRM user account
+        private static string clientId = "1bf3a99f-c5cc-47c4-97cd-3b8e6fc47ed5";
+        private static string UserName = "friyank@Fapple.onmicrosoft.com";
+        private static string Password = "dell@123";
+
         static void Main(string[] args)
         {
+            HttpMessageHandler messageHandler;
+            messageHandler = new AuthenitcateCaller(serviceUrl, clientId,
+                        new HttpClientHandler(), UserName, Password);
+
+            using (HttpClient httpClient = new HttpClient(messageHandler))
+            {
+                httpClient.BaseAddress = new Uri(serviceUrl);
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                httpClient.DefaultRequestHeaders.Add("Prefer", "odata.maxpagesize=3");
+                httpClient.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
+                httpClient.Timeout = new TimeSpan(0, 2, 0);
+                //var response = httpClient.GetAsync("api/data/v8.1/accounts?$select=name,revenue&$filter=revenue lt 100000",
+                //HttpCompletionOption.ResponseHeadersRead).Result;
+                var response = httpClient.GetAsync("api/data/v8.1/contacts?$select=lastname,address1_line1",
+                     HttpCompletionOption.ResponseHeadersRead).Result;
+                Console.WriteLine(response.StatusCode);
+                if (response.IsSuccessStatusCode)
+                {
+                    JObject body = JObject.Parse(response.Content.ReadAsStringAsync().Result);
+                    foreach (var item in body["value"])
+                    {
+                        Console.WriteLine("Your system user ID is: {0}", item["address1_line1"]);
+                        Console.WriteLine("Your system user ID is: {0}", item["lastname"]);
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("The request failed with a status of '{0}'",
+                           response.ReasonPhrase);
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
---
+++ WebApiHelper/AuthenitcateCaller .cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace WebApiHelper
+{
+    public class AuthenitcateCaller : DelegatingHandler
+    {
+        private AuthenticationHeaderValue authHeader;
+        public AuthenitcateCaller(string serviceUrl, string clientId, HttpMessageHandler innerHandler, string UserName, string Password)
+            : base(innerHandler)
+        {
+            // Obtain the Azure Active Directory Authentication Library (ADAL) authentication context.
+            AuthenticationParameters ap = AuthenticationParameters.CreateFromResourceUrlAsync(
+                    new Uri(serviceUrl + "api/data/")).Result;
+            AuthenticationContext authContext = new AuthenticationContext(ap.Authority, false);
+
+            //Note that an Azure AD access token has finite lifetime, default expiration is 60 minutes.
+            Task<AuthenticationResult> authResult = authContext.AcquireTokenAsync(serviceUrl, clientId, new UserPasswordCredential(UserName, Password));
+            authHeader = new AuthenticationHeaderValue("Bearer", authResult.Result.AccessToken);
+
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(
+                 HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            request.Headers.Authorization = authHeader;
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
